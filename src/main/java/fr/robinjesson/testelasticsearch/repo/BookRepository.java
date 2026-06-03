77bdb1530a4249fc1af6e931d19b36d916403b72
Robin <robin.jesson@hotmail.fr>
2026-06-03 22:14:48 +0200

    add custom query method to BookRepository for multi-field search

--- src/main/java/fr/robinjesson/testelasticsearch/repo/BookRepository.java
+++ src/main/java/fr/robinjesson/testelasticsearch/repo/BookRepository.java
@@ -1,12 +1,20 @@
 package fr.robinjesson.testelasticsearch.repo;
 
 import fr.robinjesson.testelasticsearch.model.opensearch.BookDocument;
+import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
 import java.util.List;
 
 public interface BookRepository extends ElasticsearchRepository<BookDocument, Long> {
-    List<BookDocument> findByTitleContainingOrContentContaining(String title, String content);
 
-    List<BookDocument> findByAuthor(String author);
+    @Query("""
+        {
+          "multi_match": {
+            "query": "?0",
+            "fields": ["title", "content"],
+            "operator": "OR"
+          }
+        }
+        """)
+    List<BookDocument> findByTitleContainingOrContentContaining(String title, String content);
 }
